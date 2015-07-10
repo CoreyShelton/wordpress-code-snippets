757e2d0b76936bee73da7a4f3758cec5de376512
Corey Shelton <coreyds@gmail.com>
2015-07-10 16:52:57 -0400

    Add basic plugin page

--- plugins/ajax-test-plugin/ajax-test.php
+++ plugins/ajax-test-plugin/ajax-test.php
@@ -2,15 +2,17 @@
 
 /**
  * Plugin Name: Ajax Test
- * Plugin URI: http://danielpataki.com
+ * Plugin URI: Corey Shelton
  * Description: This is a plugin that allows you to test Ajax functionality in WordPress
  * Version: 1.0.0
- * Author: Daniel Pataki
- * Author URI: http://danielpataki.com
+ * Author: Corey Shelton
+ * Author URI: http://hicoreyshelton.com
  * License: GPL2
  */
  
-add_action( 'wp_enqueue_scripts', 'ajax_test_enqueue_scripts' );
+/**
+ * Enqueue JavaScript and localize Wordpress Ajax Object
+ */
 function ajax_test_enqueue_scripts() {
     wp_enqueue_script( 'test', plugins_url( '/test.js', __FILE__ ), array('jquery'), '1.0', true );
     wp_localize_script( 'test', 'ajax_object', array(
@@ -18,11 +20,44 @@ function ajax_test_enqueue_scripts() {
         'nextNonce' => wp_create_nonce( 'rating-nonce' ))
     );
 }
-/*
-	Example URL: http://www.abmg-dev.com/wp-admin/admin-ajax.php?action=get_post_content&nextNonce=10d28ab002&id=1744
-*/
-add_action ( 'wp_ajax_nopriv_get_post_content', 'get_post_content' );
-add_action ( 'wp_ajax_get_post_content', 'get_post_content' );
+add_action( 'wp_enqueue_scripts', 'ajax_test_enqueue_scripts' );
+
+
+/**
+ * Create Ajax call
+ */
+function get_latest() {
+    $args = array(
+        'posts_per_page'  => 5,
+        'category'        => 1,
+    );
+    
+    $posts_array = get_posts($args);
+    
+    echo '<nav role="navigation">';
+    
+    echo '<h2>Latest News</h2>';
+    
+    echo '<ul>';
+    
+    foreach ($posts_array as $post):
+        setup_postdata($post);
+        echo '<li><a class="' . esc_attr("ajax-post") . '" href="' . esc_attr(get_page_link($post->ID)) . '" data-postID="'. esc_attr($post->ID) .'">' . $post->post_title . '</a>';
+    endforeach;
+    
+    echo '</ul>';
+    
+    echo '</nav>';
+    
+    die();
+}
+add_action( 'wp_ajax_nopriv_get_latest', 'get_latest' );
+add_action( 'wp_ajax_get_latest', 'get_latest' );
+
+
+/**
+ * Get post content based upon the ID being passed via $_GET
+ */
 function get_post_content () {
     
     $nonce = !empty($_GET['nextNonce']) ? $_GET['nextNonce'] : 0; 
@@ -55,4 +90,19 @@ function get_post_content () {
     endif;
     
 }
-?>
+add_action ( 'wp_ajax_nopriv_get_post_content', 'get_post_content' );
+add_action ( 'wp_ajax_get_post_content', 'get_post_content' );
+
+
+/** 
+ * Add Plugin Page
+ * @link https://blog.idrsolutions.com/2014/06/wordpress-plugin-part-1/
+ */
+function ajax_test_init(){
+    echo "<h1>Hello World!</h1>";
+}
+
+function ajax_test_plugin_setup_menu(){
+    add_menu_page( 'Ajax Test Plugin Page', 'Ajax Test Plugin', 'manage_options', 'ajax-test-plugin', 'ajax_test_init' );
+}
+add_action('admin_menu', 'ajax_test_plugin_setup_menu');
